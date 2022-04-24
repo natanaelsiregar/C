39ad38e623781697b8a1e1c6b70b551629437f57
Natanael Jansudin Siregar <89391887+natanaelsiregar@users.noreply.github.com>
2022-04-25 01:19:50 +0700

    Create helloworld.c

---
+++ 01. Basic/helloworld.c
@@ -0,0 +1,15 @@
+/**
+ * Simple C output program, display string: Hello World
+ * @author natgharz
+ */
+  
+// Header file for input output functions
+#include <stdio.h>
+  
+// main function (execution of program begins)
+int main()
+{
+    printf("Hello World");      // print the string inside quotation
+  
+    return 0;
+}
